87977ee6470e7d4d5dfc5f1554792305a6eb4507
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2022-12-16 23:33:08 -0300

    prog

--- Udemy/aula12.py
+++ Udemy/aula12.py
@@ -1,10 +1,12 @@
 
 
-print('Calculo de IMC')
+print('~*' * 5,'Calculo de IMC', '*~' * 5 )
 nome = input('Digite seu nome: ')
-altura = input('Por favor, informe sua aultura(em metros): ')
-peso = input('Agora, insira seu peso:')
+altura = float(input('Por favor, informe sua altura(em metros): '))
+peso = float(input('Agora, insira seu peso: '))
 
 imc = peso/(altura**2)
 
-print(f'{nome} tem {peso} de altura, pesa {peso}kgs e seu IMC é {imc}')
+print('~*' * 15)
+
+print(f'{nome} tem {altura} de altura, pesa {peso}kgs e seu IMC é {imc:.2f}.')
