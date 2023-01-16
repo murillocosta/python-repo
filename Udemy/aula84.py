8ca506e84b962c28e083258bb0133ad74cce5348
Murillo AL Costa <91096652+murillocosta@users.noreply.github.com>
2023-01-16 14:30:53 -0300

    prog

--- Udemy/aula84.py
+++ Udemy/aula84.py
@@ -22,3 +22,14 @@ novos_produtos = [{**produto, 'preco': produto['preco'] * 1.05} if produto['prec
                   for produto in produtos]
 
 print(*novos_produtos, sep='\n')
+
+
+lista = [n * 20 for n in range(10) if n < 5]
+print('lista', lista)
+
+
+produtos_filtrados = [
+    produto for produto in produtos if produto['preco'] > 20
+]
+
+print('Produtos filtrados', produtos_filtrados)
